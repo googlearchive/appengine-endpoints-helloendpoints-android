98dac8144a075928350565a7c493ea7b8851b08f
Paul Rashidi <PaulRashidi@acm.org>
2013-09-04 10:46:59 -0700

    Adding Copyright/License header.

--- HelloEndpointsProject/HelloEndpoints/src/main/java/com/google/devrel/samples/helloendpoints/MainActivity.java
+++ HelloEndpointsProject/HelloEndpoints/src/main/java/com/google/devrel/samples/helloendpoints/MainActivity.java
@@ -1,6 +1,20 @@
+/* Copyright 2013 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.google.devrel.samples.helloendpoints;
 
-import android.R.layout;
 import android.accounts.Account;
 import android.accounts.AccountManager;
 import android.content.Intent;
@@ -12,7 +26,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.widget.ArrayAdapter;
-import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -22,7 +35,6 @@ import com.google.android.gms.auth.GoogleAuthUtil;
 import com.google.android.gms.common.AccountPicker;
 import com.google.api.client.googleapis.extensions.android.gms.auth.GoogleAccountCredential;
 import com.google.api.services.helloworld.Helloworld;
-import com.google.api.services.helloworld.Helloworld.Greetings;
 import com.google.api.services.helloworld.Helloworld.Greetings.Authed;
 import com.google.api.services.helloworld.Helloworld.Greetings.GetGreeting;
 import com.google.api.services.helloworld.Helloworld.Greetings.ListGreeting;
@@ -33,7 +45,6 @@ import com.google.common.base.Strings;
 
 import java.io.IOException;
 import java.util.Arrays;
-import java.util.Collection;
 import java.util.List;
 import java.util.Set;
 
